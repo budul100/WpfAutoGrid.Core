f108285ba5ea664feb76233ac21554d64dff411c
Marek Grochowski <marekgrochowski@web.de>
2021-01-04 16:20:49 +0100

    Unnecessary code removed

--- WpfAutoGrid.Core.Examples/App.xaml.cs
+++ WpfAutoGrid.Core.Examples/App.xaml.cs
@@ -10,13 +10,5 @@ namespace AutoGridExamples
 {
     public partial class App : Application
     {
-        //protected override void OnStartup(StartupEventArgs e)
-        //{
-        //    base.OnStartup(e);
-        //    if (true)
-        //    {
-        //        this.Shutdown();
-        //    }
-        //}
     }
 }
